4ccb45962bb696b2c5ab033eb5e7c9f62a3f9d67
jnager <julielnager@gmail.com>
2016-06-28 20:46:17 -0700

    Started setup_board function

--- chess.py
+++ chess.py
@@ -11,6 +11,18 @@ class Board(object):
         self.board = board
 
     def is_in_check(color):
+        pass
+
+    def setup_board(self):
+        colors = ["white", "black"]
+        for color in colors:
+            if color == "white":
+                y = 1
+            else:
+                y = 6
+            for x in xrange(8):
+                pawn = Pawn(color)
+                pawn.place_on_board(self, (y, x))
 
 
 class Piece(object):
@@ -20,6 +32,32 @@ class Piece(object):
         self.color = color
 
     def place_on_board(self, board, location):
+        """
+           board should be the actual Board object for modification
+           location should be a tuple indicating (y, x)
+        """
         board.board[location[0]][location[1]] = self
 
 
+class Pawn(Piece):
+    """Inherits from Piece. Represents a Pawn"""
+
+
+class Knight(Piece):
+    """Inherits from Piece. Represents a Knight."""
+
+
+class Bishop(Piece):
+    """Inherits from Piece. Represents a Bishop."""
+
+
+class Rook(Piece):
+    """Inherits from Piece. Represents a Rook."""
+
+
+class Queen(Piece):
+    """Inherits from Piece. Represents a Queen."""
+
+
+class King(Piece):
+    """Inherits from Piece. Represents a King."""
